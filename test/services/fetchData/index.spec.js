a186048f666750869b16e7f4f6401ba2f9ef004e
SirYummy <micahbole.n@gmail.com>
2018-02-15 15:06:54 -0600

    bug : trying to intercept HTTP requests in unit tests but failing

--- test/services/fetchData/index.spec.js
+++ test/services/fetchData/index.spec.js
@@ -1,32 +1,46 @@
-import expect from 'code'
+import { expect } from 'code'
 import sinon from 'sinon'
+import fetch from 'isomorphic-fetch'
 import fetchData from '../../../src/services/fetchData'
 
-describe('Given the `fetchData` service', () => {
-    describe('when it is called', () => {
+describe('Given the `fetchData` service', function() {
+    describe('when it is called', function() {
+        let requests
         let mockUrl
-        let fetch
+        let fetchSpy
         let systemUnderTest
         
-        beforeEach(() => {
-            mockUrl = 'http://localhost'
-            fetch = sinon.spy()
+        beforeEach(function() {
+            this.server = sinon.createFakeServer()
+            mockUrl = 'http://localhost/my/mock/url.json'
+            this.server.respondWith(
+                'GET', 
+                mockUrl,
+                [ 200,
+                  {'Content-Type' : 'application/json' },
+                  '[{ "foo" : "bar" }]'
+                ]
+            )
+            fetchSpy = sinon.spy(fetch)
             systemUnderTest = fetchData(mockUrl)
+            this.server.respond()
         })
 
-        afterEach(() => {
-           
+        afterEach(function() {
+           fetchSpy.reset()
+           this.server.restore()
         })
 
-        xit('subsequently calls the `isomorphic-fetch` function with the supplied URL', () => {
-            expect(fetch.calledOnce).to.be.true()
-            expect(fetch.calledWithExactly(mockUrl)).to.be.true()
+        it('subsequently calls the `isomorphic-fetch` function with the supplied URL', function() {
+            expect(
+                fetchSpy.calledOnce && 
+                fetchSpy.calledWithExactly(mockUrl)
+            ).to.be.true()
         })
 
         xit('immediately returns a Promise', () => {
             expect(systemUnderTest).to.be.an.instanceof(Promise)
         })
-
     })
     describe('when it receives a successful response from the server', () => {
         it('fulfills the promise')
