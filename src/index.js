a048732df2768b9865e3d8d140be922e3c7ddb0e
SirYummy <micahbole.n@gmail.com>
2018-02-16 14:54:26 -0600

    chore : clean up main entry point

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import fetch from 'isomorphic-fetch';
-
 // Note: this is the entry point for the entire application
 
 // step 1: you will need to load the pizza data. it is available at /pizza.json. what-wg fetch is pre-installed.
